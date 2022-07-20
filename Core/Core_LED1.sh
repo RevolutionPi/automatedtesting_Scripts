a2722d505d2ac48ae3d6b57a47aa1d03fee3f06e
Ramiro Gsponer <r.gsponer@kunbus.com>
2022-07-20 13:10:10 +0200

    Fix Core_LED1.sh

--- Core/Core_LED1.sh
+++ Core/Core_LED1.sh
@@ -3,12 +3,10 @@
 source "Tests_Format.sh"
 
 TEST_NAME="Test Core LED-1"
+Test_NOK=0
 
 #Ausgabe wird in F_Ist gespeichert
 F_Ist=/tmp/Core_LED1_Output_IST
-
-#Ausgabe sollte wie F_Soll sein
-F_Soll=Core_LED1_Output_SOLL
 
 TXT_OK="*** *** *** Test LED-1 OK *** *** ***"
 TXT_NOK="*** *** *** Test LED-1 NOK!!! *** *** ***"
@@ -16,18 +14,61 @@ TXT_NOK="*** *** *** Test LED-1 NOK!!! *** *** ***"
 OUTPUT_OK="${Bold}${Black}${OnGreen}$TXT_OK${Rst}"
 OUTPUT_NOK="${Bold}${Black}${OnRed}$TXT_NOK${Rst}"
 
+LEDs_Core_tree=(
+	"├── a1_green -> ../../devices/platform/leds/leds/a1_green"
+	"├── a1_red -> ../../devices/platform/leds/leds/a1_red"
+	"├── a2_green -> ../../devices/platform/leds/leds/a2_green"
+	"├── a2_red -> ../../devices/platform/leds/leds/a2_red"
+	"├── default-on -> ../../devices/virtual/leds/default-on"
+	"├── mmc0 -> ../../devices/virtual/leds/mmc0"
+	"└── power_red -> ../../devices/platform/leds/leds/power_red"
+)
+
 echo -e "$TXT_Section1_Yellow"
 echo -e "${Bold}${Yellow}$TEST_NAME${Rst}"
 echo -e "$TXT_Section1_Yellow"
 
 tree /sys/class/leds > $F_Ist
 
-diff $F_Ist $F_Soll
-rv=$?
+cat $F_Ist | grep "${LEDs_Core_tree[0]}"
+if [ $? -ne 0 ]
+then
+	Test_NOK=1
+fi
+cat $F_Ist | grep "${LEDs_Core_tree[1]}"
+if [ $? -ne 0 ]
+then
+	Test_NOK=1
+fi
+cat $F_Ist | grep  "${LEDs_Core_tree[2]}"
+if [ $? -ne 0 ]
+then
+	Test_NOK=1
+fi
+cat $F_Ist | grep "${LEDs_Core_tree[3]}"
+if [ $? -ne 0 ]
+then
+	Test_NOK=1
+fi
+cat $F_Ist | grep "${LEDs_Core_tree[4]}"
+if [ $? -ne 0 ]
+then
+	Test_NOK=1
+fi
+cat $F_Ist | grep "${LEDs_Core_tree[5]}"
+if [ $? -ne 0 ]
+then
+	Test_NOK=1
+fi
+cat $F_Ist | grep "${LEDs_Core_tree[6]}"
+if [ $? -ne 0 ]
+then
+	Test_NOK=1
+fi
 
 echo -e "\n\n\n"
 
-if [ $rv == 0 ]
+if [ $Test_NOK == 0 ]
 then
 	echo -e "$TXT_Section3_Green"
 	echo -e "$OUTPUT_OK"
@@ -41,5 +82,3 @@ fi
 rm $F_Ist
 
 echo -e "$Test_Ending"
-
-#TODO - Verbesserung: Das gleiche ohne Dateien => Siehe Variablen F_Ist - F_Soll
